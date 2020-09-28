9be50c40079e69cd3ef82661bcc374ab09ef407d
LearnerSamuelX <samxiezx1115@gmail.com>
2020-09-28 15:32:58 -0400

    Updates

---
+++ precache-manifest.2cddba2b3331fe88ed7affb92a659bee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23766a54ceb3b3a4e5a7b44d481e49e3",
+    "url": "/fancyCalc/index.html"
+  },
+  {
+    "revision": "7f8b917466e7e077842f",
+    "url": "/fancyCalc/static/css/main.3fbeaceb.chunk.css"
+  },
+  {
+    "revision": "bd4d6fd3858ed9c79e9d",
+    "url": "/fancyCalc/static/js/2.6145f54a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fancyCalc/static/js/2.6145f54a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f8b917466e7e077842f",
+    "url": "/fancyCalc/static/js/main.d5c92244.chunk.js"
+  },
+  {
+    "revision": "98788625871d35349552",
+    "url": "/fancyCalc/static/js/runtime-main.be02a880.js"
+  }
+]);
